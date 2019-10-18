d7852eef6570b3bcb48320750563f0fa20b22d86
Javi Martín <javim@elretirao.net>
2019-10-18 16:24:09 +0200

    Don't let valuators update investments
    There were some confusing definitions regarding the valuation of budget
    investments.
    
    In the controller, `CommentableActions` was included, which includes the
    update action.
    
    In the abilities, a valuator was given permission to update an
    investment.
    
    However, the action to update an investment didn't work because there is
    no route defined to do so.
    
    The ability was defined so valuators could access the "edit" action,
    which will not call the "update" action but the "valuate" action. Since
    internally "edit" and "update" use the same permission, it worked.
    
    But then we added permission for regular users to update budget
    investments, and these permissions were allowing valuators to update
    another user's investment.
    
    After this change, everything seems to work properly since we check
    authorization in the controller itself instead of using abilities.

--- app/models/abilities/valuator.rb
+++ app/models/abilities/valuator.rb
@@ -5,9 +5,9 @@ module Abilities
     def initialize(user)
       valuator = user.valuator
 
-      can [:read, :update, :comment_valuation], Budget::Investment, id: valuator.assigned_investment_ids
+      can [:read, :comment_valuation], Budget::Investment, id: valuator.assigned_investment_ids
       can [:valuate], Budget::Investment, { id: valuator.assigned_investment_ids, valuation_finished: false }
-      cannot [:update, :valuate, :comment_valuation], Budget::Investment, budget: { phase: "finished" }
+      cannot [:valuate, :comment_valuation], Budget::Investment, budget: { phase: "finished" }
     end
   end
 end
