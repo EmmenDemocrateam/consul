307ab130fb87fcc6d102c1e30b4e0af98c9c0a58
decabeza <alberto@decabeza.es>
2020-06-02 13:38:33 +0200

    Hide milestones link on results page

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -110,7 +110,7 @@ module BudgetsHelper
     {
       results:    t("budgets.results.link"),
       stats:      t("stats.budgets.link"),
-      executions: t("budgets.executions.link")
+      #executions: t("budgets.executions.link")
     }.select { |section, _| can?(:"read_#{section}", budget) }.map do |section, text|
       {
         text: text,
