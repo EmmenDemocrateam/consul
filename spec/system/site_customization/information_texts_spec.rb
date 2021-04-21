744bf2073ba435c3fb61b46a9931efc24d75652d
decabeza <alberto@decabeza.es>
2021-04-21 14:59:41 +0200

    Hide section help on debates index

--- spec/system/site_customization/information_texts_spec.rb
+++ spec/system/site_customization/information_texts_spec.rb
@@ -16,12 +16,12 @@ describe "Custom information texts" do
     fill_in "contents[content_#{proposal_key}]values[value_en]", with: "Custom help about proposals"
     click_button "Save"
 
-    visit debates_path
-
-    within("#section_help") do
-      expect(page).to have_content "Custom help about debates"
-      expect(page).not_to have_content "Help about debates"
-    end
+    # visit debates_path
+    #
+    # within("#section_help") do
+    #   expect(page).to have_content "Custom help about debates"
+    #   expect(page).not_to have_content "Help about debates"
+    # end
 
     visit proposals_path
 
