e7e253fcccea266ea57a8622e7da6271ec21ee76
decabeza <alberto@decabeza.es>
2021-05-25 12:07:04 +0200

    Show phases dates on budgets only if are present

--- spec/models/budget/phase_spec.rb
+++ spec/models/budget/phase_spec.rb
@@ -35,6 +35,12 @@ describe Budget::Phase do
         expect(informing_phase).to be_valid
       end
 
+      it "is valid when dates are blank" do
+        informing_phase.assign_attributes(starts_at: "", ends_at: "")
+
+        expect(informing_phase).to be_valid
+      end
+
       it "is not valid when dates are equal" do
         informing_phase.assign_attributes(starts_at: Date.current, ends_at: Date.current)
 
